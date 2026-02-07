0dbe4df78d1e677b74d56b687d5a02220899bdad
donarazhar <donarazhar@gmail.com>
2026-02-07 09:03:36 +0700

    Update API URL to taaruf-api.donarazhar.site and add docs

--- web/src/config/api.js
+++ web/src/config/api.js
@@ -1,6 +1,6 @@
 // API Configuration
 // Change this URL based on environment
-const API_BASE_URL = 'https://api.taaruf.donarazhar.site';
+const API_BASE_URL = 'https://taaruf-api.donarazhar.site';
 
 // For local development, uncomment below:
 // const API_BASE_URL = 'http://localhost:8000';
